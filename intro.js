caa7715bf8f9fa07d76cc388a71e740043f5d533
HarshNegi0927 <harshnegi0927@gmail.com>
2024-12-23 01:52:22 +0000

    comiitted

---
+++ intro.js
@@ -0,0 +1,3 @@
+var user = { name: "harsh", age: 10 };
+console.log(user.name);
+console.log(user.age);
---
+++ intro.ts
@@ -0,0 +1,3 @@
+let user = {name:"harsh",age:10}
+console.log(user.name);
+console.log(user.age);
