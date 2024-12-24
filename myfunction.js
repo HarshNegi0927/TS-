ebca73f1a489fdd79e6fecb95b79c04a77a64e33
HarshNegi0927 <harshnegi0927@gmail.com>
2024-12-24 01:58:51 +0000

    commited

---
+++ myfunction.js
@@ -0,0 +1,15 @@
+function addTwo(num) {
+    return num + 2;
+    //return "hello"
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid) { }
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+var myValue = addTwo(5);
+getUpper("harsh");
+signUpUser("harsh", "harsh.gmail.com", false);
+loginUser("harsh", "harsh.com");
---
+++ variables.ts
@@ -0,0 +1,10 @@
+let greetings: string = "hello harsh";
+console.log(greetings);
+
+//number
+let UserId: number = 334455
+
+//boolean
+let isLoggedIn: boolean = false
+
+export {}
